5a53f4352e75d7907a07037ffd692fee2d2fe71e
Diego Guilherme <diegoguilherme752@oultook.com>
2022-10-27 14:25:18 -0300

    feat: observer

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,8 @@
 /* Desenvolva seu script aqui */
 
-import { getNews, AccessNews } from "../../scripts/api.js";
-import { setLocalStorage, getLocalStorage } from "../../scripts/getAndSetLocalStorage.js";
-import { renderNews } from "../../scripts/render.js";
-
-const page = 1
-
-const ListNews = await getNews(page)
-const News = await AccessNews(ListNews[0].id)
+import { observe } from "../../scripts/observer.js";
 
 
-renderNews(ListNews)
+const divObsever = document.querySelector(".container-observer")
 
+observe.observe(divObsever)
